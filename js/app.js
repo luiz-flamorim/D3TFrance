1a60ff29712e870ca138a6caaadb9bfceeed0af3
luiz-flamorim <luiz.flamorim@gmail.com>
2021-01-08 19:23:34 -0300

    finally working!

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 let margin = {
     top: 30,
     right: 30,
-    bottom: 30,
+    bottom: 150,
     left: 30,
 }
 let width = 960 - margin.left - margin.right;
@@ -12,26 +12,36 @@ let svg = d3.select('#viz')
     .attr('width', width + margin.left + margin.right)
     .attr('height', height + margin.top + margin.bottom)
     .append('g')
-    .attr('transofrm', `translate(${margin.left}, ${margin.top})`);
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 let x = d3.scaleBand()
     .range([0, width])
     .padding(0.1)
+
+let y = d3.scaleLinear()
+    .range([height, 0])
 
 d3.csv('sales.csv', (d) => {
         d.sales = +d.sales;
         return d
     })
     .then((results) => {
-        x.domain(results.map(d => d.flavours))
+        x.domain(results.map(d => d.flavors))
+        y.domain([0, d3.max(results, d => d.sales)])
+            .nice()// rounds up the value for the axis
 
-        let y = d3.scaleLinear()
-            .range([height, 0])
-            .domain([0, d3.max(results, d => d.sales)])
-            .nice()
-
-        svg.append('g')
+        svg.append('g')// y axis
             .call(d3.axisLeft(y))
+
+        svg.append('g')// x axis
+            .attr('transform', `translate(0, ${height})`)
+            .call(d3.axisBottom(x))
+            .selectAll('text')
+            .attr('x', x.bandwidth()/2)
+            .attr('y', 0)
+            .attr('dy', '0.35em')
+            .attr('transform', 'rotate(90)')
+            .attr('text-anchor', 'start')
 
     })
     .catch((error) => {
